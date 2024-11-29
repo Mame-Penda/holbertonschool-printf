b159fbb8cd588f22835c3fbc7079d23877524c19
Mame-Penda <mamependasadio1@gmail.com>
2024-11-29 20:19:11 +0100

    fixed integer

--- print_integer.c
+++ print_integer.c
@@ -7,27 +7,29 @@
  */
 int print_integer(va_list args)
 {
-	long int num = va_arg(args, int);
-	int pow = 1;
-	int len = 0;
+	int arg = va_arg(args, int);
+	int pow = 1, len = 0;
+	unsigned int tmp_num;
 
-	if (num < 0)
+	if (arg < 0)
 	{
 		_putchar('-');
-		num = -num;
+		arg *= -1;
 		len++;
 	}
-	if (num == 0)
+	tmp_num = arg;
+
+	if (tmp_num == 0)
 	{
 		_putchar('0');
 			return (len + 1);
 	}
-	while (num / pow >= 10)
+	while (tmp_num / pow >= 10)
 		pow *= 10;
 	while (pow > 0)
 	{
-		_putchar((num / pow) + '0');
-		num %= pow;
+		_putchar((tmp_num / pow) + '0');
+		tmp_num %= pow;
 		pow /= 10;
 		len++;
 	}
